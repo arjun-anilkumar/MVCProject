359ebb1392d09ef9d052fb754c69df9f04553c33
aanilkumar <aanilkumar@SUYPC215>
2017-08-16 18:17:28 +0530

    Add project files.

---
+++ Action Filters WKSP/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Action_Filters_WKSP.MvcApplication" Language="C#" %>
---
+++ Action Filters WKSP/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Action_Filters_WKSP.Startup))]
+namespace Action_Filters_WKSP
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
